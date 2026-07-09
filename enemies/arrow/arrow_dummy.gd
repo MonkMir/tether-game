3990a26b57f7ae9a39a57a8466b3c065725aca1d
Monk <PRIVATE>
2026-07-08 19:15:54 -0500

    Add auto-target to Arrow dummy

--- enemies/arrow/arrow_dummy.gd
+++ enemies/arrow/arrow_dummy.gd
@@ -6,7 +6,11 @@ enum State {
 	SPECIAL
 }
 
-var state := State.PASSIVE
+var state := State.PASSIVE:
+	set(new_state):
+		state = new_state
+		if state == State.SPECIAL:
+			_set_target()
 #const DEFAULT_SPEED_LIMIT : int = 2000 #1200
 #const MAX_SPEED_LIMIT : int = 2000
 #const MIN_SPEED_LIMIT : int = 1000
@@ -31,35 +35,30 @@ func _physics_process(delta: float):
 				if not player.is_tethered:
 					state = State.SPECIAL
 		State.SPECIAL:
-			# built-in bool that disable's Godot's physics simulation
-			custom_integrator = true
+				rotation = linear_velocity.angle()
+
+
+
+func _set_target():
+	var previous_move_direction = linear_velocity.normalized()
+	linear_velocity = launch_speed * previous_move_direction
 			
-			var previous_move_direction = linear_velocity.normalized()
-			linear_velocity = launch_speed * previous_move_direction
-			# BUG add rotation instructions after passive angular velocity is fixed
+	var best_target : Node2D
+	var best_alignment : float = -INF
+	var auto_alignment_threshold := 0.8
+	
+	for enemy in get_tree().get_nodes_in_group("enemies"):
+		if previous_move_direction.dot(enemy.global_position) >= best_alignment:
+			best_target = enemy
+			best_alignment = previous_move_direction.dot(enemy.global_position)
+	if best_alignment >= auto_alignment_threshold:
+		var target_direction : Vector2 = global_position.direction_to(best_target.global_position)
+		linear_velocity = launch_speed * target_direction
+	else:
+		linear_velocity = launch_speed * previous_move_direction
+	
+	custom_integrator = true
 
-
-# Not functional
-#func launch_dir() -> Vector2:
-#var enemies = get_tree().get_nodes_in_group("enemies")
-#var enemyDir : Vector2
-#var selectDotProd : float = -INF
-#
-#for enemy in enemies:
-#enemyDir = (enemy.position - position).normalized()
-#var currentDotProd := direction.dot(enemyDir)
-#
-#if currentDotProd > selectDotProd and currentDotProd > 0:
-#newDir = enemyDir
-#
-#
-#if selectDotProd != -INF:
-#print("new selected!")
-#return newDir
-#
-#else:
-#print("womp")
-#return direction
 #func get_combo_speed_limit() -> float:
 #may not be in use
 #var percentToMaxCombo = inverse_lerp(0, GameState.MAX_COMBO, GameState.comboCounter)
